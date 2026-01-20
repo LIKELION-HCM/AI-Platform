4ba4181428eb1ecc78a96a16e1796c82292716e7
victor <thtien0110@likelion.net>
2026-01-20 09:28:18 +0700

    add gtag

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -3,6 +3,7 @@ import { AuthUIProvider } from "@/context/AuthUIContext";
 import "@/lib/dayjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -24,6 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0XV4L412MF"
+          strategy="afterInteractive"
+        />
+
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0XV4L412MF');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} antialiased`}>
         <AuthProvider>
           <AuthUIProvider>{children}</AuthUIProvider>
